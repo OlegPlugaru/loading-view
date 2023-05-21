44d4831240e906900bab98c59200d35bf1c73013
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-21 16:14:51 +0300

    Initial Commit

---
+++ loading-view/loading_viewApp.swift
@@ -0,0 +1,17 @@
+//
+//  loading_viewApp.swift
+//  loading-view
+//
+//  Created by Whoami on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct loading_viewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
