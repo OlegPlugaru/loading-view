b8bf3786dca05a36626d99b20de68d8b1468faf3
Oleg Plugaru <oleg.plugaru.md@gmail.com>
2023-05-22 20:41:36 +0300

    finished

--- loading-view/ContentView.swift
+++ loading-view/ContentView.swift
@@ -8,19 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var isLoading = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color(.red)
+                .ignoresSafeArea()
+            
+            Text("Example Screen")
+                .bold()
+                .font(.title)
+                .foregroundColor(.white)
+                .padding()
+            
+            if isLoading {
+                LoadingView()
+            }
         }
-        .padding()
+        .onAppear { startFakeNetworkCall() }
+    }
+    
+    func startFakeNetworkCall() {
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            isLoading = false
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct LoadingView: View {
+    var body: some View {
+        ZStack {
+            Color(.systemBackground)
+                .ignoresSafeArea()
+                .opacity(0.8)
+            
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .red))
+                .scaleEffect(3)
+        }
     }
 }
